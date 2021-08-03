ae8422ce9400018bd7a8b9911282dd5d1008bb8d
Debjit Das <64192273+debjit-2102@users.noreply.github.com>
2021-08-03 23:59:48 +0530

    Create Find the Duplicate Number.cpp

---
+++ Find the Duplicate Number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int t = nums[0];
+        int h = nums[0];
+        
+        
+        do
+        {
+            t = nums[nums[t]];
+            h = nums[h];
+        }while(t != h);
+        
+        t = nums[0];
+        
+        while(t != h)
+        {
+            t = nums[t];
+            h = nums[h];
+        }
+        
+        return h;
+        
+    }
+};
